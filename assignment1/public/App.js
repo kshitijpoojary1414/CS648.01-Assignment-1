766560f72d2041544c4dbb5b2d779a4d5ce1d15c
kshitij1414 <89951228+kshitijpoojary1414@users.noreply.github.com>
2022-01-30 18:07:18 -0800

    Added presets in package.json

--- assignment1/public/App.js
+++ assignment1/public/App.js
@@ -1,22 +1,20 @@
-"use strict";
-
-var nameComponent = /*#__PURE__*/React.createElement("div", {
+const nameComponent = /*#__PURE__*/React.createElement("div", {
   id: "out"
 }, /*#__PURE__*/React.createElement("h2", {
   id: "name_comp"
 }, "Kshitij V Poojary"));
-var pictureComponent = /*#__PURE__*/React.createElement("div", {
+const pictureComponent = /*#__PURE__*/React.createElement("div", {
   id: "picture_out"
 }, /*#__PURE__*/React.createElement("img", {
   id: "picture_comp",
   src: "./tiger.jpg"
 }));
-var introComponent = /*#__PURE__*/React.createElement("div", {
+const introComponent = /*#__PURE__*/React.createElement("div", {
   id: "intro_out"
 }, /*#__PURE__*/React.createElement("p", {
   id: "intro_comp"
 }, "Hi there, I'm Kshitij Poojary a MSCS graduate at San Diego State University."));
-var buttonComponent = /*#__PURE__*/React.createElement("div", {
+const buttonComponent = /*#__PURE__*/React.createElement("div", {
   id: "button_out"
 }, /*#__PURE__*/React.createElement("a", {
   href: "https://github.com/kshitijpoojary1414"
